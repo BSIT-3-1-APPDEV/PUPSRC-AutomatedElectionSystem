0bb7051dade63cb874d7d8ba9d4de123e4a714ec
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2024-04-17 13:55:47 +0800

    feat: Kill only the session of logged-in user

--- src/voter-logout.php
+++ src/voter-logout.php
@@ -1,7 +1,8 @@
 <?php
+require_once 'includes/session-handler.php';
 
-session_start();
-session_destroy();
+// Kill only the session of logged-in user
+// Retain the session of which database organization a user is connected to
 unset($_SESSION['voter_id']);
 header("Location: landing-page.php");
 ?>
