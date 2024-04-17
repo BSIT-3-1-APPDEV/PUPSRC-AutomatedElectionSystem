b14083624424830d744aa395aa61c8efa9264f17
ao3xjh <148093188+ao3xjh@users.noreply.github.com>
2024-04-17 13:33:28 +0800

    style: updated the landing page and login page

--- src/landing-page.php
+++ src/landing-page.php
@@ -2,21 +2,20 @@
 require_once 'includes/session-handler.php';
 
 // Check if the user is already logged in
-if(isset($_SESSION['voter_id'])) {
+if (isset($_SESSION['voter_id'])) {
   // Check if the 'role' key exists in the session
-  if(isset($_SESSION['role'])) {
-      // Redirect based on role
-      if($_SESSION['role'] == 'Student Voter') {
-          header("Location: ballot-forms.php");
-      }
-      elseif($_SESSION['role'] == 'Committee Member') {
-          header("Location: admindashboard.php");
-      }
-      exit();
+  if (isset($_SESSION['role'])) {
+    // Redirect based on role
+    if ($_SESSION['role'] == 'Student Voter') {
+      header("Location: ballot-forms.php");
+    } elseif ($_SESSION['role'] == 'Committee Member') {
+      header("Location: admindashboard.php");
+    }
+    exit();
   } else {
-      // If 'role' key does not exist, handle the error or redirect to an appropriate page
-      echo "Role not found in session.";
-      exit();
+    // If 'role' key does not exist, handle the error or redirect to an appropriate page
+    echo "Role not found in session.";
+    exit();
   }
 }
 
@@ -24,36 +23,59 @@ if(isset($_SESSION['voter_id'])) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-  <!-- Bootstrap 5 -->
-  <link type="text/css" href="../vendor/node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Fontawesome Link for Icons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="styles/dist/landing.css">
+  <link rel="icon" href="images/resc/ivote-favicon.png" type="image/x-icon">
   <title>PUP Automated Election System</title>
 </head>
 
 <body id="index-body">
+  <nav class="navbar navbar-expand-lg fixed-top">
+    <div class="container">
+      <!-- <a class="navbar-brand" href="#">Your Brand</a> -->
+      <img src="images/resc/iVOTE-Landing2.png" id="ivote-logo-landing-header" alt="ivote-logo">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About Us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="#">Register</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
   <!-- Parallax section -->
   <section class="parallax">
     <div class="container">
       <div class="row">
-        <div class="col-lg-8 offset-lg-2">
-          <div class="container-fluid text-center">
-            <img src="images/logos/pup-src.png" class="img-fluid pup-logo" alt="PUPSRC Logo">
-            <h5 id="index-PUPSRC" class="text-truncate">Polytechnic University of the Philippines - Santa Rosa Campus</h5>
-            <h1 id="index-AES">AUTOMATED ELECTION SYSTEM</h1>
-            <a href="#organizations" type="button" class="btn btn-primary fw-bold index-button">Select your Organization</a>
-          
-            <div class="index-wave-footer">
-              <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                  <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
-              </svg>
-          </div>
+        <div class="col text-center text-white">
+          <img src="images/resc/iVOTE4.png" class="img-fluid ivote-logo" alt="iVote Logo">
+          <h5 id="index-PUPSRC" class="text-truncate stroked-text">Polytechnic University of the Philippines -
+            Santa Rosa Campus</h5>
+          <h1 class="stroked-text" id="index-AES">AUTOMATED ELECTION SYSTEM</h1>
+          <a href="#organizations" type="button" class="btn btn-primary fw-bold index-button" id="">Select
+            Organization</a>
         </div>
-        </div>
+      </div>
+      <div class="index-wave-footer">
+        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+          <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+        </svg>
       </div>
     </div>
   </section>
@@ -62,124 +84,159 @@ if(isset($_SESSION['voter_id'])) {
   <section id="organizations" class="organizations">
     <form action="includes/classes/landing-page-controller.php" method="post">
       <div class="container-fluid">
-        <h2 class="landing-organization-title">Organization</h2>
+        <h2 class="landing-organization-title"><span class="hello-text">Hello,</span> Isko’t Iska!</h2>
         <p class="landing-organization-subtitle">- Select your organization - </p>
 
-        <div class="container-fluid mt-4">
+        <div class="container-fluid">
           <div class="row justify-content-center text-center">
-            <div class="col-md-3">
-              <button type="submit" name="submit_btn" value="sco"  class="landing-page-org-card" id="SCO-landing-logo">
+            <div class="col-md-3 mb-4">
+              <button type="submit" name="submit_btn" value="sco" class="landing-page-org-card" id="SCO-landing-logo">
                 <img src="images/logos/sco.png" alt="SCO Logo" class="landing-page-logo-size">
                 <h3 class="fw-bold pt-2">Student Council Organization</h3>
               </button>
-            </div> 
+            </div>
           </div>
         </div>
 
-        
-        <div class="container-fluid mt-4">
+
+        <div class="container-fluid">
           <div class="row justify-content-center text-center">
-            <div class="col-md-3" id="index-ACAP">       
+            <div class="col-md-3 mb-4" id="index-ACAP">
               <button type="submit" name="submit_btn" value="acap" class="landing-page-org-card" id="ACAP-landing-logo">
                 <img src="images/logos/acap.png" alt="ACAP Logo" class="landing-page-logo-size">
                 <h3 class="fw-bold pt-2">ACAP</h3>
               </button>
             </div>
 
-            <div class="col-md-3" id="index-AECES">
+            <div class="col-md-3 mb-4" id="index-AECES">
               <button type="submit" name="submit_btn" value="aeces" class="landing-page-org-card" id="AECES-landing-logo">
                 <img src="images/logos/aeces.png" alt="AECES Logo" class="landing-page-logo-size">
                 <h3 class="fw-bold pt-2">AECES</h3>
               </button>
             </div>
 
-            <div class="col-md-3" id="index-ELITE">
+            <div class="col-md-3 mb-4" id="index-ELITE">
               <button type="submit" name="submit_btn" value="elite" class="landing-page-org-card" id="ELITE-landing-logo">
                 <img src="images/logos/elite.png" alt="ELITE Logo" class="landing-page-logo-size">
                 <h3 class="fw-bold pt-2">ELITE</h3>
               </button>
-            </div>  
+            </div>
           </div>
         </div>
 
-        <div class="container-fluid mt-4">
+        <div class="container-fluid">
           <div class="row justify-content-center text-center">
-            <div class="col-md-3" id="index-ACAP">
+            <div class="col-md-3 mb-4" id="index-ACAP">
               <button type="submit" name="submit_btn" value="give" class="landing-page-org-card" id="GIVE-landing-logo">
                 <img src="images/logos/give.png" alt="GIVE Logo" class="landing-page-logo-size">
                 <h3 class="fw-bold pt-2">GIVE</h3>
               </button>
             </div>
-            <div class="col-md-3" id="index-JEHRA">
+            <div class="col-md-3 mb-4" id="index-JEHRA">
               <button type="submit" name="submit_btn" value="jehra" class="landing-page-org-card" id="JEHRA-landing-logo">
                 <img src="images/logos/jehra.png" alt="JEHRA Logo" class="landing-page-logo-size">
                 <h3 class="fw-bold pt-2">JEHRA</h3>
               </button>
             </div>
 
-            <div class="col-md-3" id="index-JMAP">
+            <div class="col-md-3 mb-4" id="index-JMAP">
               <button type="submit" name="submit_btn" value="jmap" class="landing-page-org-card" id="JMAP-landing-logo">
                 <img src="images/logos/jmap.png" alt="JMAP Logo" class="landing-page-logo-size">
                 <h3 class="fw-bold pt-2">JMAP</h3>
               </button>
-            </div>  
+            </div>
 
           </div>
         </div>
 
-        <div class="container-fluid mt-4">
+        <div class="container-fluid ">
           <div class="row justify-content-center text-center">
-            <div class="col-md-3" id="index-JPIA">
+            <div class="col-md-3 mb-4" id="index-JPIA">
               <button type="submit" name="submit_btn" value="jpia" class="landing-page-org-card" id="JPIA-landing-logo">
                 <img src="images/logos/jpia.png" alt="JPIA Logo" class="landing-page-logo-size">
                 <h3 class="fw-bold pt-2">JPIA</h3>
               </button>
             </div>
-            <div class="col-md-3" id="index-PIIE">
+            <div class="col-md-3 mb-4" id="index-PIIE">
               <button type="submit" name="submit_btn" value="piie" class="landing-page-org-card" id="PIIE-landing-logo">
                 <img src="images/logos/piie.png" alt="PIIE Logo" class="landing-page-logo-size">
                 <h3 class="fw-bold pt-2">PIIE</h3>
               </button>
             </div>
-            </div>  
           </div>
         </div>
-      </div>  
+      </div>
+      </div>
     </form>
   </section>
 
-  <footer class="bg-dark text-light text-center py-lg-2">
-    <div class="container">
-      <p class="copyright">&copy; BSIT 3-1 | ALL RIGHTS RESERVED 2024</p>
+  <footer class="footer">
+    <div class="custom-shape-divider-top-1713266907">
+      <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" class="shape-fill"></path>
+        <path d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z" opacity=".5" class="shape-fill"></path>
+        <path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" class="shape-fill"></path>
+      </svg>
+    </div>
+    <div class="container-fluid mt-auto py-3 footer-body">
+      <div class="row">
+        <div class="col-md-6">
+          <img src="images/resc/iVOTE4.png" class="img-fluid ivote-logo" id="footer" alt="iVote Logo">
+          <p>iVOTE is an Automated Election System (AES) for the student<br>organizations of the PUP Santa Rosa Campus.
+          </p>
+          <p class="credits-footer"><span class="hello-text">© 2024 BSIT 3-1.</span> All Rights Reserved</p>
+
+        </div>
+        <div class="col-md-3">
+          <div class="row">
+            <p class="credits-footer">Visit</p>
+            <div class="col-md-3">
+              <a href="https://www.facebook.com/thepupsrcstudentcouncil">SCO</a>
+              <a href="https://www.facebook.com/ACAPpage">ACAP</a>
+              <a href="https://www.facebook.com/OfficialAECES">AECES</a>
+            </div>
+            <div class="col-md-3">
+              <a href="https://www.facebook.com/ELITE.PUPSRC">ELITE</a>
+              <a href="https://www.facebook.com/educgive">GIVE<br></a>
+              <a href="https://www.facebook.com/JPIA.PUPSRC">JPIA</a>
+            </div>
+            <div class="col-md-3">
+              <a href="https://www.facebook.com/PUPSRCJEHRA">JEHRA</a>
+              <a href="https://www.facebook.com/piiepup">PIIE</a>
+
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <p class="credits-footer">Contact Us</p>
+          <p>Email us at <a href="mailto:ivote-pupsrc@gmail.com" class="ivote-email">ivote-pupsrc@gmail.com</a></p>
+
+
+          <p><span class="ivote-email"><a href="#">About Us</a> | <a href="#">Our Story</a></span></p>
+        </div>
+      </div>
     </div>
   </footer>
+
+  <!-- Bootstrap Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
 
   <!-- JavaScript for dynamic text change PUPSRC -->
   <script>
     const indexPUPSRC = document.getElementById('index-PUPSRC');
 
     function updateText() {
-        if (window.innerWidth <= 768) {
-            indexPUPSRC.textContent = 'PUP SANTA ROSA CAMPUS';
-        } else {
-            indexPUPSRC.textContent = 'Polytechnic University of the Philippines - Santa Rosa Campus';
-        }
+      if (window.innerWidth <= 768) {
+        indexPUPSRC.textContent = 'PUP SANTA ROSA CAMPUS';
+      } else {
+        indexPUPSRC.textContent = 'Polytechnic University of the Philippines - Santa Rosa Campus';
+      }
     }
 
     window.addEventListener('load', updateText);
     window.addEventListener('resize', updateText);
-
-    // Add event listeners to organization cards to submit the form when clicked
-    document.querySelectorAll('.landing-page-org-card').forEach(card => {
-      card.addEventListener('click', function() {
-        // Get the organization name from the card ID
-        const orgName = this.id.split('-')[0];
-        // Set the value of the hidden input field in the form
-        document.getElementById('selectedOrganization').value = orgName;
-        // Submit the form
-        document.getElementById('organizationForm').submit();
-      });
-    });
   </script>
 </body>
+
 </html>
--- src/voter-login.php
+++ src/voter-login.php
@@ -9,14 +9,13 @@ include 'includes/organization-list.php';
 $org_full_name = $org_full_names[$org_name];
 
 // Check if the user is already logged in
-if(isset($_SESSION['voter_id'])) {
+if (isset($_SESSION['voter_id'])) {
     // Check if the 'role' key exists in the session
-    if(isset($_SESSION['role'])) {
+    if (isset($_SESSION['role'])) {
         // Redirect based on role
-        if($_SESSION['role'] == 'Student Voter') {
+        if ($_SESSION['role'] == 'Student Voter') {
             header("Location: ballot-forms.php");
-        }
-        elseif($_SESSION['role'] == 'Committee Member') {
+        } elseif ($_SESSION['role'] == 'Committee Member') {
             header("Location: admindashboard.php");
         }
         exit();
@@ -31,6 +30,7 @@ if(isset($_SESSION['voter_id'])) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -39,7 +39,7 @@ if(isset($_SESSION['voter_id'])) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
 
     <!-- Bootstrap 5 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Online Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -48,19 +48,27 @@ if(isset($_SESSION['voter_id'])) {
 
     <link rel="stylesheet" href="styles/dist/landing.css">
     <link rel="stylesheet" href="styles/orgs/<?php echo $org_name; ?>.css">
+    <link rel="icon" href="images/logos/<?php echo $org_name; ?>.png" type="image/x-icon">
     <title>Login</title>
 </head>
 
-<body class="login-body" id="<?php echo strtoupper($org_name) ;?>-body">
+<body class="login-body" id="<?php echo strtoupper($org_name); ?>-body">
+    <nav class="navbar navbar-expand-lg fixed-top" id="login-navbar">
+        <div class="container-fluid d-flex justify-content-center align-items-center">
+            <img src="images/resc/iVOTE-Landing2.png" id="ivote-logo-landing-header" alt="ivote-logo">
+        </div>
+    </nav>
+
+
     <div class="container-fluid">
         <div class="row">
-            <div class="col login-left-section">
+            <div class="col-md-6 login-left-section">
                 <div class="organization-names">
-                    <img src="images/logos/<?php echo $org_name; ?>.png" class="img-fluid login-logo" alt="<?php echo strtoupper($org_name) . ' ' ; ?>Logo">
+                    <img src="images/logos/<?php echo $org_name; ?>.png" class="img-fluid login-logo" alt="<?php echo strtoupper($org_name) . ' '; ?>Logo">
                     <p><?php echo strtoupper($org_full_name); ?></p>
                     <h1 class="login-AES">AUTOMATED ELECTION SYSTEM</h1>
 
-                    <div class="login-wave-footer" id="<?php echo strtoupper($org_name) ;?>-wave">
+                    <div class="login-wave-footer" id="<?php echo strtoupper($org_name); ?>-wave">
                         <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                             <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" class="shape-fill"></path>
                             <path d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z" opacity=".5" class="shape-fill"></path>
@@ -69,17 +77,17 @@ if(isset($_SESSION['voter_id'])) {
                     </div>
                 </div>
             </div>
-            <div class="col login-right-section">
+            <div class="col-md-6 login-right-section">
                 <div>
                     <form action="voter-login-inc.php" method="post" class="login-form needs-validation" novalidate>
                         <h1 class="login-account">Login Account</h1>
                         <p>Sign in your account</p>
-                    
+
                         <div class="col-md-12 mt-4 mb-3">
                             <input type="email" class="form-control" id="Email" name="email" placeholder="Email Address" required pattern="[a-zA-Z0-9._%+-]+@gmail\.com$">
                         </div>
 
-                    
+
                         <div class="col-md-12 mb-2">
                             <div class="input-group">
                                 <input type="password" class="form-control" name="password" placeholder="Password" id="Password" required>
@@ -88,14 +96,15 @@ if(isset($_SESSION['voter_id'])) {
                                 </button>
                             </div>
                         </div>
-                    
+
                         <a href="#">Forgot Password</a>
-                    
+
                         <div class="d-grid gap-2 mt-5 mb-4">
                             <button class="btn btn-primary" name="sign_in" type="submit">Sign In</button>
                         </div>
-                    
-                        <p>Don't have an account? <a href="#" id="<?php echo strtolower($org_name) ;?>SignUP">Sign Up</a></p>
+
+                        <p>Don't have an account? <a href="#" id="<?php echo strtolower($org_name); ?>SignUP">Sign
+                                Up</a></p>
                     </form>
                 </div>
             </div>
@@ -104,46 +113,47 @@ if(isset($_SESSION['voter_id'])) {
 
     <script>
         // Example starter JavaScript for disabling form submissions if there are invalid fields
-(() => {
-  'use strict'
+        (() => {
+            'use strict'
 
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  const forms = document.querySelectorAll('.needs-validation')
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            const forms = document.querySelectorAll('.needs-validation')
 
-  // Loop over them and prevent submission
-  Array.from(forms).forEach(form => {
-    form.addEventListener('submit', event => {
-      if (!form.checkValidity()) {
-        event.preventDefault()
-        event.stopPropagation()
-      }
+            // Loop over them and prevent submission
+            Array.from(forms).forEach(form => {
+                form.addEventListener('submit', event => {
+                    if (!form.checkValidity()) {
+                        event.preventDefault()
+                        event.stopPropagation()
+                    }
 
-      form.classList.add('was-validated')
-    }, false)
-  })
-})()
+                    form.classList.add('was-validated')
+                }, false)
+            })
+        })()
     </script>
 
-<!-- Updated script for password toggle -->
-<script>
-    document.addEventListener("DOMContentLoaded", function () {
-        const togglePassword = document.querySelector("#password-toggle");
-        const passwordInput = document.querySelector("#Password");
-        const eyeIcon = togglePassword.querySelector("i");
-    
-        togglePassword.addEventListener("click", function () {
-            const type =
-                passwordInput.getAttribute("type") === "password"
-                    ? "text"
-                    : "password";
-            passwordInput.setAttribute("type", type);
-    
-            // Toggle eye icon classes
-            eyeIcon.classList.toggle("fa-eye-slash");
-            eyeIcon.classList.toggle("fa-eye");
+    <!-- Updated script for password toggle -->
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            const togglePassword = document.querySelector("#password-toggle");
+            const passwordInput = document.querySelector("#Password");
+            const eyeIcon = togglePassword.querySelector("i");
+
+            togglePassword.addEventListener("click", function() {
+                const type =
+                    passwordInput.getAttribute("type") === "password" ?
+                    "text" :
+                    "password";
+                passwordInput.setAttribute("type", type);
+
+                // Toggle eye icon classes
+                eyeIcon.classList.toggle("fa-eye-slash");
+                eyeIcon.classList.toggle("fa-eye");
+            });
         });
-    });
     </script>
 
 </body>
+
 </html>
