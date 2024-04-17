44c6973801d4d4fd8fe3a16fa17a2c648e0b0461
Biella Mariscotes <biella.requina@gmail.com>
2024-04-17 08:49:41 +0800

    chore: updated links of the sidebar

--- src/includes/components/sidebar.php
+++ src/includes/components/sidebar.php
@@ -17,37 +17,41 @@
 	<div class="menu-content">
 		<ul class="menu-items ms-xl-1">
 			<li class="item">
-				<a href="#" class="active"><i data-feather="home" class="white me-xl-3 mb-xl-1"></i>Home</a>
+				<a href="src/admindashboard.php" class="active"><i data-feather="home" class="white me-xl-3 mb-xl-1"></i>Home</a>
 			</li>
 
 			<li class="item">
-				<a href="#"><i data-feather="bar-chart-2" class="white me-xl-3 mb-xl-1"></i>Reports</a>
+				<a href="result-generation.php"><i data-feather="bar-chart-2" class="white me-xl-3 mb-xl-1"></i>Reports</a>
 			</li>
 
 			<li class="item">
 				<div class="submenu-item" data-bs-toggle="collapse" href="#firstSubmenu" id="submenuToggle"
 					data-bs-parent="false">
-					<span class="fw-semibold"><i data-feather="user-plus" class="white me-xl-3 mb-xl-1"></i>Add
-						Users</span>
+					<span class="fw-semibold"><i data-feather="users" class="white me-xl-3 mb-xl-1"></i>Manage Users</span>
 					<i class="fas fa-chevron-right" id="submenuIcon"></i>
 				</div>
 				<ul class="menu-items submenu collapse" id="firstSubmenu">
 					<li class="item">
-						<a href="#">Admin Account</a>
+						<a href="#">Voters</a>
+					</li>
+				</ul>
+				<ul class="menu-items submenu collapse" id="firstSubmenu">
+					<li class="item">
+						<a href="admin-creation.php">Committee Member</a>
 					</li>
 				</ul>
 			</li>
 
 			<li class="item">
-				<a href="#"><i data-feather="users" class="white me-xl-3 mb-xl-1"></i>Manage Voters</a>
+				<a href="#"><i data-feather="user-plus" class="white me-xl-3 mb-xl-1"></i>Add Candidate</a>
 			</li>
 
 			<li class="item">
-				<a href="#"><i data-feather="archive" class="white me-xl-3 mb-xl-1"></i>Archive</a>
+				<a href="archive.php"><i data-feather="archive" class="white me-xl-3 mb-xl-1"></i>Archive</a>
 			</li>
 
 			<li class="item">
-				<a href="#"><i data-feather="settings" class="white me-xl-3 mb-xl-1"></i>Configuration</a>
+				<a href="configuration.php"><i data-feather="settings" class="white me-xl-3 mb-xl-1"></i>Configuration</a>
 			</li>
 		</ul>
 	</div>
