14d97e0c2ed4e3ee4dc1193cced29c42ec1342d8
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2024-04-28 18:42:34 +0800

    fix: Set cookie parameter called secure to false. I believe this is what causing everyone getting logged out unexpectedly once logged in or set in session.

--- src/includes/session-handler.php
+++ src/includes/session-handler.php
@@ -1,30 +1,28 @@
 <?php
 
-// Additional security to avoid session hijacking
 ini_set('session.use_only_cookies', 1);
 ini_set('session.use_strict_mode', 1);
 
 session_set_cookie_params([
-    'lifetime' => 1800,
+    'lifetime' => 1800, // Session will last only for 30 mins
     'domain' => 'localhost',
     'path' => '/',
-    'secure' => true,
+    'secure' => false, // False because the system is being develop locally
     'httponly' => true
 ]);
 
 session_start();
 
-if(!isset($_SESSION['last_regen'])) {
+if (!isset($_SESSION['last_regeneration'])) {
     regenerateSessionId();
-}
-else {
+} else {
     $interval = 60 * 30;
-    if(time() - $_SESSION['last_regen'] >= $interval) {
+    if (time() - $_SESSION['last_regeneration'] >= $interval) {
         regenerateSessionId();
     }
 }
 
 function regenerateSessionId() {
     session_regenerate_id(true);
-    $_SESSION['last_regen'] = time();
+    $_SESSION['last_regeneration'] = time();
 }
