cc8b38f9e921ea25bb0e45e4f861179c84905e1d
Andrei Matibag <andreirovicpmatibag@iskolarngbayan.pup.edu.ph>
2024-04-29 14:03:52 +0800

    add: reformed admin creation ui

--- src/admin-creation.php
+++ src/admin-creation.php
@@ -1,119 +1,147 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+require_once 'includes/classes/db-connector.php';
+require_once 'includes/session-handler.php';
+require_once 'includes/classes/session-manager.php';
 
-<head>
-	<meta charset="UTF-8" />
-	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<link rel="icon" type="image/x-icon" href="images/resc/ivote-favicon.png">
-	<title>Admin Creation</title>
+if (isset($_SESSION['voter_id'])) {
 
-	<!-- Icons -->
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" />
-	<script src="https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js"></script>
+    // ------ SESSION EXCHANGE
+    include 'includes/session-exchange.php';
+    // ------ END OF SESSION EXCHANGE
+    ?>
+    <!DOCTYPE html>
+    <html lang="en">
 
-	<!-- UPON USE, REMOVE/CHANGE THE '../../' -->
-    <link rel="stylesheet" href="styles/styles.css" />
-	<link rel="stylesheet" href="styles/admin-creation.css" />
-	<link rel="stylesheet" href="../vendor/node_modules/bootstrap/dist/css/bootstrap.min.css" />
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" type="image/x-icon" href="images/resc/ivote-favicon.png">
+        <title>Manage Account</title>
 
-</head>
+        <!-- Icons -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" />
+        <script src="https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js"></script>
 
-<body>
+        <!-- UPON USE, REMOVE/CHANGE THE '../../' -->
+        <link rel="stylesheet" href="<?php echo 'styles/orgs/' . $org_name . '.css'; ?>" id="org-style">
+        <link rel="stylesheet" href="styles/style.css" />
+        <link rel="stylesheet" href="styles/admin-creation.css" />
+        <link rel="stylesheet" href="../vendor/node_modules/bootstrap/dist/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="../vendor/node_modules/bootstrap/dist/css/bootstrap-grid.min.css" />
 
-	<!---------- SIDEBAR + HEADER START ------------>
-	<?php include_once __DIR__ . '/includes/components/sidebar.php'; ?>
-	<!---------- SIDEBAR + HEADER END ------------>
+    </head>
+
+    <body>
+
+        <!---------- SIDEBAR + HEADER START ------------>
+        <?php include_once __DIR__ . '/includes/components/sidebar.php'; ?>
+        <!---------- SIDEBAR + HEADER END ------------>
+
+        <div class="main">
+            <div class="container mb-5 pl-5">
+                <div class="row justify-content-center">
+                    <div class="col-md-11">
+                        <div class="breadcrumbs d-flex">
+                            <button type="button" class="btn btn-lvl-white d-flex align-items-center spacing-8 fs-8">
+                                <i data-feather="users" class="white im-cust feather-2xl"></i> MANAGE USERS
+                            </button>
+                            <button type="button" class="btn btn-lvl-current rounded-pill spacing-8 fs-8">COMMITTEE
+                                MEMBERS</button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
 
 
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-12 card-box card-box-larger mt-md-10">
 
-
-
-<div class="main">
-  <h1>Create an Admin Account</h1>
-        <div class="row justify-content-center">
-            <div class="col-md-10 card-box">
-                    <div class="card-box">
-                        <div class="row">
-                            <div class="col-12">
-                                <h2 class="form-title">Personal Information</h2>
+                        <div class="container-fluid">
+                            <div class="card-box">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <h2 class="form-title">Create Admin Account</h2>
+                                    </div>
+                                </div>
+                                <br>
+                                <div class="row">
+                                    <form action="" method="post">
+                                        <div class="row">
+                                            <div class="col-md-4 col-sm-4 mx-auto">
+                                                <div class="form-group local-forms">
+                                                    <label for="last_name" class="login-danger">Last Name <span class="required"> * </span> </label>
+                                                    <input type="text" id="last_name" name="last_name"
+                                                        placeholder="Enter Last Name" required>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-4 col-sm-4 mx-auto">
+                                                <div class="form-group local-forms">
+                                                    <label for="first_name" class="login-danger">First Name<span class="required"> * </span> </label>
+                                                    <input type="text" id="first_name" name="first_name"
+                                                        placeholder="Enter First Name" required>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-4 col-sm-3 mx-auto">
+                                                <div class="form-group local-forms">
+                                                    <label for="middle_name" class="login-danger">Middle Name<span class="required"> * </span> </label>
+                                                    <input type="text" id="middle_name" name="middle_name"
+                                                        placeholder="Enter Middle Name" required>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6 col-sm-6">
+                                                <div class="form-group local-forms">
+                                                    <label for="email" class="login-danger">Email<span class="required"> * </span> </label>
+                                                    <input type="email" id="email" name="email" placeholder="Email"
+                                                        required>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6 col-sm-6">
+                                                <div class="form-group local-forms shorter-form-group">
+                                                    <label for="role" class="login-danger">Role<span class="required"> * </span> </label>
+                                                    <select id="role" name="role" required>
+                                                        <option value="Admin Member">Admin Member</option>
+                                                        <option value="Committee Member">Committee Member</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="d-flex justify-content-end">
+                                            <div class="d-flex flex-row">
+                                                <button type="reset" class="reset-button">Reset Form</button>
+                                                <button type="submit" value="Submit" class="button-create">Create
+                                                    Account</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
                             </div>
                         </div>
-                        <br>
-                        <div class="row">
-                            <div class="col-12 col-sm-4 mx-auto">
-                                <div class="form-group local-forms">
-                                    <label for="lname" class="login-danger">Last Name </label>
-                                    <input type="text" id="lname" name="lname" placeholder="Enter Last Name">
-                                </div>
-                            </div>
-                            <div class="col-12 col-sm-4 mx-auto">
-                                <div class="form-group local-forms">
-                                    <label for="fname" class="login-danger">First Name </label>
-                                    <input type="text" id="fname" name="fname" placeholder="Enter First Name">
-                                </div>
-                            </div>
-                            <div class="col-12 col-sm-3 mx-auto">
-                                <div class="form-group local-forms">
-                                    <label for="mname" class="login-danger">Middle Name </label>
-                                    <input type="text" id="mname" name="mname" placeholder="Enter Middle Name">
-                                </div>
-                            </div>
-                            <div class="col-12 col-sm-1 mx-auto">
-                                <div class="form-group local-forms">
-                                    <label for="suffix" class="login-danger">Suffix </label>
-                                    <select id="suffix" name="suffix">
-                                        <option value="suffix">Suffix</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="col-12">
-                                <h2 class="form-title">Admin Account Credentials</h2>
-                            </div>
-                            <div class="col-12 col-sm-12 mx-auto">
-                                <div class="form-group local-forms">
-                                    <label for="email" class="login-danger">Email </label>
-                                    <input type="email" id="email" name="email" placeholder="Email">
-                                </div>
-                            </div>
-                            <div class="col-12 col-sm-12 mx-auto">
-                                <div class="form-group local-forms">
-                                    <label for="password" class="login-danger">Password </label>
-                                    <input type="password" id="password" name="password" placeholder="Password">
-                                </div>
-                            </div>
-                            <div class="col-12 col-sm-12 mx-auto">
-                                <div class="form-group local-forms">
-                                    <label for="cpassword" class="login-danger">Confirm Password </label>
-                                    <input type="password" id="cpassword" name="cpassword" placeholder="Confirm Password">
-                                </div>
-                            </div>
-                            <div class="d-flex justify-content-center">
-                                <div class="d-flex flex-column">
-                                    <button type="submit" class="button-create">Create</button>
-                                    <button style="background: none; border: none; padding-top: 10px; padding-bottom: 20px; font-weight: bold; color: #909090; text-decoration: underline;">
-                                    Reset Form
-                                    </button>
-
-                                </div>
-                            </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
 
 
+            <?php include_once __DIR__ . '/includes/components/footer.php'; ?>
 
+            <!-- UPON USE, REMOVE/CHANGE THE '../../' -->
+            <script src="vendor/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+            <script src="scripts/script.js"></script>
+            <script src="scripts/feather.js"></script>
+            <script src="scripts/viewport.js"></script>
+            <script src="scripts/configuration.js"></script>
 
-	<!-- UPON USE, REMOVE/CHANGE THE '../../' -->
-	<script src="vendor/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-	<script src="scripts/script.js"></script>
-	<script src="scripts/feather.js"></script>
-	<script src="scripts/viewport.js"></script>
-	<script src="scripts/configuration.js"></script>
+    </body>
 
-</body>
+    </html>
 
-</html>
+    <?php
+} else {
+    header("Location: landing-page.php");
+}
+?>
