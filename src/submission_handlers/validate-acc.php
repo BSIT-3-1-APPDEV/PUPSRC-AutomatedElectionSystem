e83d0f1c422e8163efcdd666d09d43bf9d8c6526
Biella Mariscotes <biella.requina@gmail.com>
2024-05-01 01:16:22 +0800

    add: added sending of email functionality

--- src/submission_handlers/validate-acc.php
+++ src/submission_handlers/validate-acc.php
@@ -1,14 +1,17 @@
 <?php
-include_once str_replace('/', DIRECTORY_SEPARATOR, '../includes/classes/file-utils.php');
-require_once FileUtils::normalizeFilePath('../includes/classes/db-connector.php');
-require_once FileUtils::normalizeFilePath('../includes/session-handler.php');
-require_once FileUtils::normalizeFilePath('../includes/classes/session-manager.php');
-require_once FileUtils::normalizeFilePath('../includes/classes/query-handler.php');
-require_once FileUtils::normalizeFilePath('../includes/mailer.php');
-require_once FileUtils::normalizeFilePath('../includes/classes/email-sender.php');
+require_once '../includes/classes/db-connector.php';
+require_once '../includes/session-handler.php';
+require_once '../includes/classes/session-manager.php';
+require_once '../includes/classes/query-handler.php';
+require_once '../includes/mailer.php';
+require_once '../includes/classes/email-sender.php';
 
 if (isset($_POST['voter_id'])) {
+if (isset($_POST['voter_id'])) {
     $voterManager = new VoterManager();
+    $emailSender = new EmailSender($mail);
+    $voter = new Voter();
+
     $emailSender = new EmailSender($mail);
     $voter = new Voter();
 
@@ -16,6 +19,7 @@ if (isset($_POST['voter_id'])) {
     $action = $_POST['action'];
 
     if ($action == 'approve') {
+        // Updates status in database
         // Updates status in database
         $approve_query = "UPDATE voter SET status = 'Active' WHERE voter_id = $voter_id";
         $voterManager->validateVoter($voter_id, $approve_query);
@@ -25,11 +29,26 @@ if (isset($_POST['voter_id'])) {
         $emailSender->sendApprovalEmail($recipientEmail);
         $conn->close();
 
+
+        // Sending of email
+        $recipientEmail = $voter->getEmailById($voter_id);
+        $emailSender->sendApprovalEmail($recipientEmail);
+        $conn->close();
+
     } elseif ($action == 'reject') {
+
+        // Updates status in database
 
         // Updates status in database
         $reject_query = "UPDATE voter SET status = 'Rejected' WHERE voter_id = $voter_id";
         $voterManager->validateVoter($voter_id, $reject_query);
+
+        // Sending of email
+        $recipientEmail = $voter->getEmailById($voter_id);
+        $reason = isset($_POST['reason']) ? $_POST['reason'] : '';
+        $otherReason = isset($_POST['otherReason']) ? $_POST['otherReason'] : '';
+        $emailSender->sendRejectionEmail($recipientEmail, $reason, $otherReason);
+        $conn->close();
 
         // Sending of email
         $recipientEmail = $voter->getEmailById($voter_id);
@@ -43,4 +62,5 @@ if (isset($_POST['voter_id'])) {
 } else {
     echo json_encode(['success' => false, 'message' => 'Voter ID not provided']);
 }
+?>
 ?>
