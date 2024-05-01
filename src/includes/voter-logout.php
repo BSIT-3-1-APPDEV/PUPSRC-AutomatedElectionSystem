d8c83559b29fc6b06ea05cf4d4471216ffc0ce9b
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2024-05-01 15:56:21 +0800

    chore: Move to includes folder

---
+++ src/includes/voter-logout.php
@@ -0,0 +1,26 @@
+<?php
+include_once str_replace('/', DIRECTORY_SEPARATOR, __DIR__ . '/classes/file-utils.php');
+require_once FileUtils::normalizeFilePath('session-handler.php');
+
+$referer = $_SERVER['HTTP_REFERER'];
+if ($referer && strpos($referer, $_SERVER['HTTP_HOST']) !== false) {
+
+    // Kill only the session of logged-in user
+    // Retain the session of which database organization a user is connected
+    unset($_SESSION['voter_id']);
+    
+    // Redirect back to previously stored URL
+    if (isset($_SESSION['return_to'])) {
+        $return_to = $_SESSION['return_to'];
+        unset($_SESSION['return_to']);
+        header("Location: $return_to");
+    } else {
+        header("Location: ../landing-page.php");
+    }
+    exit;
+} 
+else {   
+    header("Location: ../landing-page.php");
+    exit;
+}
+?>
