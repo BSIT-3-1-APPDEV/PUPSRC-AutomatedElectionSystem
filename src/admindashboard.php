0058c2da8843d10c584d7f0cb02a68092d267810
Peter-Escueta <peterwilrexemescueta@iskolarngbayan.pup.edu.ph>
2024-05-03 10:48:29 +0800

    style: changed favicon

--- src/admindashboard.php
+++ src/admindashboard.php
@@ -42,6 +42,7 @@ if (isset($_SESSION['voter_id']) && $_SESSION['role'] == 'Committee Member') {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="images/resc/ivote-favicon.png">
     <title>Admin Dashboard</title>
 
     <!-- Montserrat Font -->
