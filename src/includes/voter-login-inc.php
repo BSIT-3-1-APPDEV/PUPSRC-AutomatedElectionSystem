eedd336311de9f0d73b7ccf3f3c557417da13522
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2024-05-20 18:43:22 +0800

    fix: AT7-98-Check if an organization or a user is already set in session before authentication

--- src/includes/voter-login-inc.php
+++ src/includes/voter-login-inc.php
@@ -5,6 +5,13 @@ require_once FileUtils::normalizeFilePath('classes/voter-login-controller.php');
 require_once FileUtils::normalizeFilePath('classes/voter-login-class.php');
 require_once FileUtils::normalizeFilePath('error-reporting.php');
 
+if(isset($_SESSION['voter_id']) || $_SESSION['organization'] == NULL) {
+    $_SESSION['error_message'] = "Due to unusual activities we've detected from you. Your organization session was terminated.";
+    unset($_SESSION['voter_id']);
+    header("Location: ../landing-page.php");
+    exit();
+}
+
 // Check if CSRF token isn't both set and submitted thru POST method
 if(!isset($_POST['csrf_token']) || !isset($_SESSION['csrf_token'])) {
     displayUnsetToken();
@@ -40,7 +47,7 @@ if($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['sign-in'])) {
 }
 
 function displayUnsetToken() {
-    $_SESSION['error_message'] = 'Something went wrong.<br/>Please reload the page.';
+    $_SESSION['error_message'] = 'Something went wrong. Please reload the page.';
     header("Location: ../voter-login.php");
     exit();
 }
