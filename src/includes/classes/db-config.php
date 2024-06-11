e204fddbe7eb9ee649dd22274a016fbfc6a91482
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2024-06-11 12:34:22 +0800

    Change database configuration to go live.

--- src/includes/classes/db-config.php
+++ src/includes/classes/db-config.php
@@ -4,13 +4,15 @@ require_once FileUtils::normalizeFilePath(__DIR__ . '/../error-reporting.php');
 require_once FileUtils::normalizeFilePath(__DIR__ . '/../session-handler.php');
 
 class DatabaseConfig {
+    private $prefix = 'u155023598_';
+
     public static function getOrganizationDBConfig ($organization) {
         return array(
             'host' => 'localhost',
-            'username' => 'root',
-            'password' => '',
+            'username' => $prefix . $organization,
+            'password' => 'Student_0rg',
             // Adds 'db_' prefix to access specific org database
-            'database' => 'db_' . $organization
+            'database' => $prefix . 'db_' . $organization
         );
     }
 }
