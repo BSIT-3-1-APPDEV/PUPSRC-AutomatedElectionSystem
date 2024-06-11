b9439275354446ebe0b2bfa39b02e10d86562aad
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2024-06-11 12:44:18 +0800

    Change prefix variable to static type.

--- src/includes/classes/db-config.php
+++ src/includes/classes/db-config.php
@@ -4,15 +4,15 @@ require_once FileUtils::normalizeFilePath(__DIR__ . '/../error-reporting.php');
 require_once FileUtils::normalizeFilePath(__DIR__ . '/../session-handler.php');
 
 class DatabaseConfig {
-    private $prefix = 'u155023598_';
+    private static $prefix = 'u155023598_';
 
     public static function getOrganizationDBConfig ($organization) {
         return array(
             'host' => 'localhost',
-            'username' => $prefix . $organization,
+            'username' => self::prefix . $organization,
             'password' => 'Student_0rg',
             // Adds 'db_' prefix to access specific org database
-            'database' => $prefix . 'db_' . $organization
+            'database' => self::prefix . 'db_' . $organization
         );
     }
 }
