6d11ae762cb5c327c00d0057f2031d8bf49ad188
___Sundae <jennymaelbaldoza@iskolarngbayan.pup.edu.ph>
2024-06-15 19:05:15 +0800

    chore: updating draft page header for configuration
    Updates to configuration page header but is a placeholder will wait for official design and contents

--- src/includes/views/configuration/configuration-page-title.php
+++ src/includes/views/configuration/configuration-page-title.php
@@ -1,3 +1,10 @@
-<div class="d-flex px-0">
-    <h4 class="page-title text-truncate mx-auto">Configuration</h2>
+'<div class="d-flex px-0">
+    <!-- <h4 class="page-title text-truncate mx-auto">Configuration</h4> -->
+
+    <div class="card page-title text-truncate col-12">
+        <div class="card-body">
+            <h3 class="">Configuration</h3>
+            <small class="">Welcome to configuration.</small>
+        </div>
+    </div>
 </div>
