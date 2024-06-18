06c212c16eb35e47e8abf34cf787860780663935
ivanedan <ivanangelopedan@iskolarngbayan.pup.edu.ph>
2024-06-19 03:27:00 +0800

    fix: added modals and linking of setting pages

--- src/user-setting-information.php
+++ src/user-setting-information.php
@@ -106,7 +106,7 @@ if (isset($_SESSION['voter_id']) && (isset($_SESSION['role'])) && ($_SESSION['ro
                     <i class="fas fa-exchange-alt me-4" style="font-size: 1.1rem;"></i>
                     <div>
                       <div class="mb-0" style="font-size: 18px;">
-                        <b><a href="../src/transfer-org.php" class="custom-link">Transfer Org</a></b>
+                        <b><a href="../src/user-setting-transfer.php" class="custom-link">Transfer Org</a></b>
                       </div>
                       <div class="mb-0 des">Move your account to a different organization upon transfer.</div>
                     </div>
@@ -143,14 +143,14 @@ if (isset($_SESSION['voter_id']) && (isset($_SESSION['role'])) && ($_SESSION['ro
                   <a href="<?php echo "user_data/$org_name/cor/" . $row['cor']; ?>" download class="d-inline-flex align-items-center">
                   </a>
                   <p class="filename"><i data-feather="paperclip" class="white im-cust feather-2xs"></i>RegistrationCertificate.pdf</p>
-                    
+
                   <div class="tools">
-                  <span class="fs-7 ps-sm-2 spacing-3 fw-medium download">
+                    <span class="fs-7 ps-sm-2 spacing-3 fw-medium download">
                       <i class="fas fa-download fa-sm"></i> Download
                     </span>
                     <i class="fa-solid fa-expand fa-sm fullscreen-icon"></i>
                   </div>
-                  
+
                   <br>
                   <br>
                 </div>
@@ -161,6 +161,110 @@ if (isset($_SESSION['voter_id']) && (isset($_SESSION['role'])) && ($_SESSION['ro
       </div>
       </div>
 
+      <!-- Modals -->
+      <div class="modal fade" id="successResetPasswordLinkModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content d-flex align-items-center justify-content-center" id="success-modal">
+            <div class="modal-body text-center w-100">
+              <div class="col-md-12">
+                <img src="images/resc/icons/shield.png" class="change-passs-modal-icon" alt="iVote Logo">
+              </div>
+              <p class="fw-bold fs-4 change-password-title spacing-4 mt-3">Enter Password</p>
+              <p class="change-password-sub">To change your email address, please provide your current password.</p>
+              <form class="needs-validation" id="forgot-password-form" name="forgot-password-form" novalidate enctype="multipart/form-data">
+                <div class="col-12 col-md-12">
+                  <div class="input-group">
+                    <input type="password" class="form-control mx-auto align-self-center" id="change-password" name="change-password" onkeypress="return avoidSpace(event)" placeholder="Type current password here...">
+                    <button class="btn btn-secondary eye-toggle" type="button" id="password-toggle-1" style="display: none;">
+                      <i class="fas fa-eye-slash"></i>
+                    </button>
+                  </div>
+                </div>
+                <div class="col-md-12 mt-5">
+                  <button type="button" class="btn btn-secondary cancel-button" data-bs-dismiss="modal" id="cancel-modal">Cancel</button>
+                  <button type="button" class="btn btn-primary delete-button" id="submit-button">Submit</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <!-- Success Modal -->
+      <div class="modal" id="approvalModal" data-bs-keyboard="false" data-bs-backdrop="static">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div class="modal-body">
+              <div class="d-flex justify-content-end">
+                <i class="fa fa-solid fa-circle-xmark fa-xl close-mark light-gray custom-margin" onclick="redirectToPage('user-setting-information.php?voter_id=<?php echo htmlspecialchars($row["voter_id"]); ?>')"></i>
+                </i>
+              </div>
+              <div class="text-center">
+                <div class="col-md-12">
+                  <img src="images/resc/check-animation.gif" class="check-perc" alt="iVote Logo">
+                </div>
+
+                <div class="row">
+                  <div class="col-md-12 pb-3">
+                    <p class="fw-bold fs-3 success-color spacing-4">Email Verification Sent</p>
+                    <p class="fw-medium spacing-5">We sent a message to your current email.<br> Please follow the instructions to change <br> your email address.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!--Max Attempts Reach Modal -->
+      <div class="modal" id="errorModal" data-bs-keyboard="false" data-bs-backdrop="static">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div class="modal-body">
+              <div class="d-flex justify-content-end">
+                <i class="fa fa-solid fa-circle-xmark fa-xl close-mark light-gray custom-margin" id="onlyErrorClose">
+                </i>
+              </div>
+              <div class="text-center">
+                <div class="col-md-12">
+                  <img src="images/resc/warning.png" alt="Warning Icon">
+                </div>
+
+                <div class="row">
+                  <div class="col-md-12 pb-3 pt-4">
+                    <p class="fw-bold fs-3 danger spacing-4 px-2" style="color: #EE1C24;">Max Attempts Reached</p>
+                    <p class="fw-medium spacing-5 pt-2 px-5 ">Sorry, you've reached the maximum number <br>
+                      of attempts to change your password. For <br> security reasons, please wait for <b>30 minutes</b> <br> before trying again.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- JavaScript to show approval modal after submission -->
+      <script>
+        document.addEventListener('DOMContentLoaded', function() {
+          document.getElementById('submit-button').addEventListener('click', function() {
+            // Assuming the submission is successful, show the approval modal
+            var approvalModal = new bootstrap.Modal(document.getElementById('approvalModal'));
+            approvalModal.show();
+          });
+        });
+
+        // JS for the submit approval modal when the user click submit the modal is closed
+        document.getElementById('submit-button').addEventListener('click', function() {
+          // Perform any form validation or submission logic here
+
+          // If the form is valid, close the modal
+          var myModalEl = document.getElementById('successResetPasswordLinkModal');
+          var modal = bootstrap.Modal.getInstance(myModalEl); // Returns a Bootstrap modal instance
+          modal.hide();
+        });
+      </script>
     </main>
     <div class="footer">
       <?php include_once __DIR__ . '/includes/components/footer.php'; ?>
