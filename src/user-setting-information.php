d00e4317dffbafc0e20a37ceb99d694cdfb66542
ivanedan <ivanangelopedan@iskolarngbayan.pup.edu.ph>
2024-06-19 02:56:27 +0800

    style: new format for the container of the information setting module

--- src/user-setting-information.php
+++ src/user-setting-information.php
@@ -40,6 +40,24 @@ if (isset($_SESSION['voter_id']) && (isset($_SESSION['role'])) && ($_SESSION['ro
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" />
     <script src="https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <style>
+      .nav-link:hover,
+      .nav-link:focus {
+        color: var(--main-color);
+      }
+
+      .navbar-nav .nav-item.dropdown.show .nav-link.main-color {
+        color: var(--main-color);
+      }
+
+      .navbar-nav .nav-item.dropdown .nav-link.main-color,
+      .navbar-nav .nav-item.dropdown .nav-link.main-color:hover,
+      .navbar-nav .nav-item.dropdown .nav-link.main-color:focus {
+        color: var(--main-color);
+      }
+    </style>
+
   </head>
 
   <body>
@@ -49,197 +67,101 @@ if (isset($_SESSION['voter_id']) && (isset($_SESSION['role'])) && ($_SESSION['ro
     include_once FileUtils::normalizeFilePath(__DIR__ . '/includes/components/topnavbar.php');
     ?>
 
-    <div class="m-4 d-flex justify-content-center">
-      <div class="row" style="width: 100%;margin-top: 60px;margin-bottom: 60px; display: flex; justify-content: center; align-content: center;">
-        <div class="col-lg-3 col-md-2 col-12">
-          <div class="reminder">
-            <br>
-            <div class="main-color main-text ps-5 pe-4">
-              <br>
-              <div>
-                <h4><strong>Settings & Privacy</strong></h4>
-              </div>
-              <br>
-            </div>
-          </div>
-          <br>
-          <div class="reminder">
-            <div class="vertical-icons">
-              <i data-feather="user" class=" main-color white im-cust feather-2l"></i>
-              <i data-feather="lock" class="white im-cust feather-2l"></i>
-              <i data-feather="repeat" class="white im-cust feather-2l"></i>
-            </div>
-            <div class="font-weight1">
-              <div class="pt-4">
-                <div class="sub-head main-color ps-4 pe-4">
-                  <h5>Information</h5>
-                </div>
-                <div class="main-color detail ps-4 pe-4 mt-1">
-                  <p>See your account information like your email address and certificate of registration.</p>
-                </div>
-                <br>
-                <div class="sub-head ps-4 pe-4">
-                  <h5>Change Password</h5>
-                </div>
-                <div class="detail ps-4 pe-4 mt-1">
-                  <p>Ensure your account's security by updating your password whenever you need.</p>
-                </div>
-                <br>
-                <div class="sub-head ps-4 pe-4">
-                  <h5>Transfer Org</h5>
-                </div>
-                <div class="detail ps-4 pe-4 mt-1">
-                  <p>Move your account to different organization upon transfer.</p>
-                </div>
+    <main>
+      <div class="container" style="margin-top: 5%; margin-bottom:5%;">
+        <div class="row">
+          <!-- left side -->
+          <div class="col-lg-3 mb-4 pe-lg-3">
+            <div class="row pb-3">
+              <div class="px-4 pt-4 pb-3 title main-color text-center spacing">
+                <h5><b>Settings & Privacy</b></h5>
               </div>
             </div>
-          </div>
-          <br>
-        </div>
-        <div class="col-lg-8" style="width: 800px;">
-          <div class="reminder2">
             <div class="row">
-              <div class="col">
-                <div class="font-weight1">
-                  <div class="pt-5"></div>
-                  <div class="sub-head2 main-color ps-4 pe-4">
-                    <h5><i data-feather="user" class="white im-cust feather-2l"></i>Information</h5>
+              <div class="p-4 title" style="font-size: 12.8px;">
+                <div class="px-2">
+                  <div class="main-color d-flex align-items-center pt-2 pb-4">
+                    <div class="pe-4">
+                      <i data-feather="user" class="white" style="width: 20px; height: 20px;"></i>
+                    </div>
+                    <div>
+                      <div class="mb-0" style="font-size: 18px; font-weight:600">
+                        <a href="../src/user-setting-information.php" class="custom-link"> Information </a>
+                      </div>
+                      <div class="mb-0 des">See your account information like your email address and certificate of registration.</div>
+                    </div>
                   </div>
-                  <div class="rounded-icon main-bg-color">
-                    <i data-feather="mail" class="white im-cust feather-4l"></i>
+                  <div class="d-flex align-items-center pb-4">
+                    <div class="pe-4">
+                      <i data-feather="lock" class="white" style="width: 20px; height: 20px;"></i>
+                    </div>
+                    <div>
+                      <div class="mb-0" style="font-size: 18px; font-weight:600">
+                        <a href="../src/user-setting-password.php" class="custom-link"> Change Password </a>
+                      </div>
+                      <div class="mb-0 des">Ensure your account's security by updating your password whenever you need.</div>
+                    </div>
                   </div>
-                  <div class="container-data">
-                    <h4 class="email-add">Email Address</h4>
-                    <p class="user-email-1">kimMingyu@gmail.com</p>
-                    <hr class="email-border">
-                    <button type="button" class="main-color transparent-btn" id="changePasswordBtn" data-bs-toggle="modal" data-bs-target="#successResetPasswordLinkModal">Change Email Address</button>
-                    </br>
+                  <div class="d-flex align-items-center pb-4">
+                    <i class="fas fa-exchange-alt me-4" style="font-size: 1.1rem;"></i>
+                    <div>
+                      <div class="mb-0" style="font-size: 18px;">
+                        <b><a href="../src/transfer-org.php" class="custom-link">Transfer Org</a></b>
+                      </div>
+                      <div class="mb-0 des">Move your account to a different organization upon transfer.</div>
+                    </div>
                   </div>
                 </div>
-                <div class="col" style="margin-top: -200px; margin-left: 350px;">
-                  <iframe id="pdfViewer" src="<?php echo "user_data/$org_name/cor/" . $row['cor']; ?>" class="right-aligned-iframe" frameborder="0"></iframe>
-                </div>
-                <a href="<?php echo "user_data/$org_name/cor/" . $row['cor']; ?>" download class="d-inline-flex align-items-center">
-                </a>
-                <p class="filename"><i data-feather="paperclip" class="white im-cust feather-2xs"></i>RegistrationCertificate.pdf</p>
-                <div class="container">
-                  <span class="fs-7 ps-sm-2 spacing-5 fw-medium download">
-                    <i class="fas fa-download fa-sm"></i> Download
-                  </span>
-                  <i class="fa-solid fa-expand fa-sm fullscreen-icon"></i>
+              </div>
+            </div>
+          </div>
+          <!-- right side -->
+          <div class="col-lg-9 ps-lg-4">
+            <div class="row">
+              <div class="p-4 title" style="font-size:15px;">
+                <div class="py-3 px-2 px-lg-4 px-sm-1">
+                  <h5 class="main-color pb-2">
+                    <b>
+                      <i data-feather="user" class="fas fa-exchange-alt me-4" style="font-size: 1rem;"></i>Information
+                    </b>
+                  </h5>
+                  <div id="section-1" style="align-items: center; justify-content: center;">
+                    <div class="rounded-icon main-bg-color">
+                      <i data-feather="mail" class="white im-cust feather-4l"></i>
+                    </div>
+                    <div class="container-data">
+                      <h4 class="email-add">Email Address</h4>
+                      <p class="user-email-1">kimMingyu@gmail.com</p>
+                      <hr class="email-border">
+                      <button type="button" class="main-color transparent-btn" id="changePasswordBtn" data-bs-toggle="modal" data-bs-target="#successResetPasswordLinkModal">Change Email Address</button>
+                      </br>
+                    </div>
+                  </div>
+                  <div class="col" style="margin-top: -200px; margin-left: 400px;">
+                    <iframe id="pdfViewer" src="<?php echo "user_data/$org_name/cor/" . $row['cor']; ?>" class="right-aligned-iframe" frameborder="0"></iframe>
+                  </div>
+                  <a href="<?php echo "user_data/$org_name/cor/" . $row['cor']; ?>" download class="d-inline-flex align-items-center">
+                  </a>
+                  <p class="filename"><i data-feather="paperclip" class="white im-cust feather-2xs"></i>RegistrationCertificate.pdf</p>
+                    
+                  <div class="tools">
+                  <span class="fs-7 ps-sm-2 spacing-3 fw-medium download">
+                      <i class="fas fa-download fa-sm"></i> Download
+                    </span>
+                    <i class="fa-solid fa-expand fa-sm fullscreen-icon"></i>
+                  </div>
+                  
+                  <br>
+                  <br>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    <!-- Modals -->
-    <div class="modal fade" id="successResetPasswordLinkModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-      <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content d-flex align-items-center justify-content-center" id="success-modal">
-          <div class="modal-body text-center w-100">
-            <div class="col-md-12">
-              <img src="images/resc/icons/shield.png" class="change-passs-modal-icon" alt="iVote Logo">
-            </div>
-            <p class="fw-bold fs-4 change-password-title spacing-4 mt-3">Enter Password</p>
-            <p class="change-password-sub">To change your email address, please provide your current password.</p>
-            <form class="needs-validation" id="forgot-password-form" name="forgot-password-form" novalidate enctype="multipart/form-data">
-              <div class="col-12 col-md-12">
-                <div class="input-group">
-                  <input type="password" class="form-control mx-auto align-self-center" id="change-password" name="change-password" onkeypress="return avoidSpace(event)" placeholder="Type current password here...">
-                  <button class="btn btn-secondary eye-toggle" type="button" id="password-toggle-1" style="display: none;">
-                    <i class="fas fa-eye-slash"></i>
-                  </button>
-                </div>
-              </div>
-              <div class="col-md-12 mt-5">
-                <button type="button" class="btn btn-secondary cancel-button" data-bs-dismiss="modal" id="cancel-modal">Cancel</button>
-                <button type="button" class="btn btn-primary delete-button" id="submit-button">Submit</button>
-              </div>
-            </form>
-          </div>
-        </div>
       </div>
-    </div>
 
-
-    <!-- Success Modal -->
-    <div class="modal" id="approvalModal" data-bs-keyboard="false" data-bs-backdrop="static">
-      <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-          <div class="modal-body">
-            <div class="d-flex justify-content-end">
-              <i class="fa fa-solid fa-circle-xmark fa-xl close-mark light-gray custom-margin" onclick="redirectToPage('user-setting-information.php?voter_id=<?php echo htmlspecialchars($row["voter_id"]); ?>')"></i>
-              </i>
-            </div>
-            <div class="text-center">
-              <div class="col-md-12">
-                <img src="images/resc/check-animation.gif" class="check-perc" alt="iVote Logo">
-              </div>
-
-              <div class="row">
-                <div class="col-md-12 pb-3">
-                  <p class="fw-bold fs-3 success-color spacing-4">Email Verification Sent</p>
-                  <p class="fw-medium spacing-5">We sent a message to your current email.<br> Please follow the instructions to change <br> your email address.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <!--Max Attempts Reach Modal -->
-    <div class="modal" id="errorModal" data-bs-keyboard="false" data-bs-backdrop="static">
-      <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-          <div class="modal-body">
-            <div class="d-flex justify-content-end">
-              <i class="fa fa-solid fa-circle-xmark fa-xl close-mark light-gray custom-margin" id="onlyErrorClose">
-              </i>
-            </div>
-            <div class="text-center">
-              <div class="col-md-12">
-                <img src="images/resc/warning.png" alt="Warning Icon">
-              </div>
-
-              <div class="row">
-                <div class="col-md-12 pb-3 pt-4">
-                  <p class="fw-bold fs-3 danger spacing-4 px-2" style="color: #EE1C24;">Max Attempts Reached</p>
-                  <p class="fw-medium spacing-5 pt-2 px-5 ">Sorry, you've reached the maximum number <br>
-                    of attempts to change your password. For <br> security reasons, please wait for <b>30 minutes</b> <br> before trying again.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- JavaScript to show approval modal after submission -->
-    <script>
-      document.addEventListener('DOMContentLoaded', function() {
-        document.getElementById('submit-button').addEventListener('click', function() {
-          // Assuming the submission is successful, show the approval modal
-          var approvalModal = new bootstrap.Modal(document.getElementById('approvalModal'));
-          approvalModal.show();
-        });
-      });
-
-      // JS for the submit approval modal when the user click submit the modal is closed
-      document.getElementById('submit-button').addEventListener('click', function() {
-        // Perform any form validation or submission logic here
-
-        // If the form is valid, close the modal
-        var myModalEl = document.getElementById('successResetPasswordLinkModal');
-        var modal = bootstrap.Modal.getInstance(myModalEl); // Returns a Bootstrap modal instance
-        modal.hide();
-      });
-    </script>
-
-
+    </main>
     <div class="footer">
       <?php include_once __DIR__ . '/includes/components/footer.php'; ?>
     </div>
